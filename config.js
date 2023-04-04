52f28fd430d9869bbc37835533962503130ee225
Peter <peter_s_96@live.com>
2023-04-04 12:55:23 -0700

    somewhat complete

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: "mongodb://localhost:27017",
+  MONGODB_URL: "mongodb+srv://peter96:123123a@clusterfinal.oml8jqs.mongodb.net/?retryWrites=true&w=majority",
   JWT_SECRET: "don't tell a soul",
 };
